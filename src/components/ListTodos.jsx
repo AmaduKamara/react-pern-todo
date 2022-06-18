daf03a4df55d0fe5405c582b63ff8eb010fe3469
Amkam <amaduskamara@gmail.com>
2022-06-18 02:59:39 +0000

    Create delete todo function and called on delete btn

--- src/components/ListTodos.jsx
+++ src/components/ListTodos.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 
+// Fetching all todos
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
 
@@ -13,9 +14,22 @@ const ListTodos = () => {
     }
   };
 
+  // useEffect calling the fetch todo function on render
   useEffect(() => {
     fetchTodos();
   }, []);
+
+  // Deletinga todo
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+      window.location = "/";
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
   return (
     <>
@@ -34,8 +48,13 @@ const ListTodos = () => {
                 todos.map((todo) => (
                   <tr key={todo.todo_id}>
                     <td>{todo.description}</td>
-                    <td className="btn">Edit</td>
-                    <td className="btn">Delete</td>
+                    <td className="text-info btn">Edit</td>
+                    <td
+                      className="text-danger btn"
+                      onClick={() => handleDelete(todo.todo_id)}
+                    >
+                      Delete
+                    </td>
                   </tr>
                 ))}
             </tbody>
